e51fc287b893d14500c0ba97687f7777b78772e0
wiz <aenor.realm@gmail.com>
2013-02-07 16:03:31 +0200

    Fancy ops dump.

--- src/wsdl2hs/Main.hs
+++ src/wsdl2hs/Main.hs
@@ -3,15 +3,17 @@
 module Main where
 
 import qualified Data.ByteString.Lazy.Char8 as BSL
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
 import           Network.HTTP.Conduit (simpleHttp)
 import           Text.XML (parseLBS_)
 import           Data.Default (def)
 import           Text.Show.Pretty (ppShow)
 import           Options.Applicative
 
-import           Control.Monad (when)
+import           Control.Monad (when, forM_)
 
-import           Web.SOAP.Schema (parseSchema, Schema(..))
+import           Web.SOAP.Schema
 
 data Opts = Opts { useFile :: Maybe String
                  , useHTTP :: Maybe String
@@ -19,21 +21,21 @@ data Opts = Opts { useFile :: Maybe String
                  } deriving (Show)
 
 data Command = Dump
-             | List
+             | ListOps
              deriving (Show)
 
 opts :: Parser Opts
 opts = Opts <$> (optional $ strOption ( short 'f' <> long "file" <> help "Load schema from a file." <> metavar "FILE"))
             <*> (optional $ strOption ( short 'h' <> long "http" <> help "Get schmea from an URL." <> metavar "URL"))
-            <*> subparser ( (command "dump" $ info dumpOpts $ progDesc "Dump intermediate schema AST representation.")
-                         <> (command "list" $ info listOpts $ progDesc "List schema elements.")
+            <*> subparser ( (command "dump"       $ info dumpOpts $ progDesc "Dump intermediate schema AST representation.")
+                         <> (command "operations" $ info listOps  $ progDesc "List service operations.")
                           )
 
 dumpOpts :: Parser Command
 dumpOpts = pure Dump
 
-listOpts :: Parser Command
-listOpts = pure List
+listOps :: Parser Command
+listOps = pure ListOps
 
 optParser :: ParserInfo Opts
 optParser = info (helper <*> opts) (fullDesc <> progDesc "Lol" <> header "wsdl2hs - a lol program")
@@ -52,8 +54,40 @@ run Opts{..} = do
     let schema = parseSchema doc
 
     case optCommand of
-        Dump -> putStrLn (ppShow schema)
-        List -> doList schema
+        Dump    -> putStrLn (ppShow schema)
+        ListOps -> doList schema
 
 doList :: Schema -> IO ()
-doList = undefined
+doList (Schema (Service sn ps) bs ts) = do
+    T.putStr "╭┅ "
+    T.putStrLn sn
+
+    forM_ (rEnum bs) $ \(bi, Binding bn bt btr bops) -> do
+        T.putStrLn . T.concat $ [node bi, "───┬╼ ", bn, " <", bt, "> ", soapTransport btr]
+        forM_ (rEnum bops) $ \(oi, Operation{..}) -> do
+            T.putStrLn . T.concat $ [bar bi, "   ", node oi, "───┬╼ ", operationName]
+
+            when (operationName /= operationAction) $
+                T.putStrLn . T.concat $ [bar bi, "   ", bar oi, "   ├──┄ ", operationAction]
+
+            case operationDocumentation of
+                Just doc -> T.putStrLn . T.concat $ [bar bi, "   ", bar oi, "   ├──“", doc, "”"]
+                Nothing -> return ()
+
+            T.putStrLn . T.concat $ [bar bi, "   ", bar oi, "   ╘══╍ ", operationInput, " →  ", operationOutput]
+
+rEnum :: [a] -> [(Int, a)]
+rEnum stuff = zip [l, l-1 .. 0] stuff
+  where l = length stuff - 1
+
+node :: Int -> T.Text
+node 0 = "╰"
+node _ = "├"
+
+bar :: Int -> T.Text
+bar 0 = " "
+bar _ = "│"
+
+soapTransport :: T.Text -> T.Text
+soapTransport "http://schemas.xmlsoap.org/soap/http" = "[HTTP]"
+soapTransport x = x
