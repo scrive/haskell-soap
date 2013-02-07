b2010ef2934c9ea88bc03a0c63f420836be4c69f
wiz <aenor.realm@gmail.com>
2013-02-07 14:25:10 +0200

    use optparse-applicative for options

--- src/wsdl2hs/Main.hs
+++ src/wsdl2hs/Main.hs
@@ -1,25 +1,39 @@
-{--# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
 
 module Main where
 
+import qualified Data.ByteString.Lazy.Char8 as BSL
 import           Network.HTTP.Conduit (simpleHttp)
-import qualified Data.Text.Lazy.IO as TL
-import           Text.XML (parseLBS_, parseText_)
+import           Text.XML (parseLBS_)
 import           Data.Default (def)
-
-import           System.Environment (getArgs)
 import           Text.Show.Pretty (ppShow)
+import           Options.Applicative
+
+import           Control.Monad (when)
 
 import           Web.SOAP.Schema (parseSchema)
 
-main = do
-    args <- getArgs
-    case args of
-        "-u":url:stuff -> (parseLBS_ def `fmap` simpleHttp url) >>= process stuff
-        "-f":fname:stuff -> (parseText_ def `fmap` TL.readFile fname) >>= process stuff
-        _ -> help
+data Opts = Opts { useFile :: Maybe String
+                 , useHTTP :: Maybe String
+                 , dump :: Bool
+                 } deriving (Show)
 
-process stuff doc =
-    putStrLn . ppShow $ parseSchema doc
+opts :: Parser Opts
+opts = Opts <$> (optional $ strOption ( short 'f' <> long "file" <> help "Load schema from a file." <> metavar "FILE"))
+            <*> (optional $ strOption ( short 'h' <> long "http" <> help "Get schmea from an URL." <> metavar "URL"))
+            <*> switch ( short 'd' <> long "dump" <> help "Dump intermediate schema AST representation.")
 
-help = putStrLn "wsdl2hs -u <URL> | -f <FILE>"
+optParser = info (helper <*> opts) (fullDesc <> progDesc "Lol" <> header "wsdl2hs - a lol program")
+
+main = execParser optParser >>= run
+
+run Opts{..} = do
+    doc <- case (useHTTP, useFile) of
+        (Just url, Nothing)   -> parseLBS_ def `fmap` simpleHttp url
+        (Nothing, Just fname) -> parseLBS_ def `fmap` BSL.readFile fname
+        (Just _, Just _)      -> error "Conflicting source provided."
+        (Nothing, Nothing)    -> error "No source specified."
+
+    let schema = parseSchema doc
+
+    when dump $ putStrLn (ppShow schema)
