9c564ca49b094617d9df13bf99202a5ba9141fe0
wiz <aenor.realm@gmail.com>
2013-04-11 17:25:10 +0300

    some real tests for mock transport and 3 cursor parsers.

--- soap/test/Main.hs
+++ soap/test/Main.hs
@@ -1,31 +1,78 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Network.SOAP
-import Network.SOAP.Transport.HTTP.Conduit
+import Network.SOAP.Parsing.Cursor
+import qualified Network.SOAP.Transport.Mock as Mock
+import qualified Network.SOAP.Transport.HTTP.Conduit as HTTP
+import Text.XML
 import Text.XML.Writer
+import Text.XML.Cursor hiding (element)
 import Text.XML.Stream.Parse as Parse
-import Data.Text (Text, unpack)
 
-main = do
-    -- Initial one-time preparations.
-    transport <- initTransport_ "http://localhost:10000/soap/endpoint"
+import Data.Text (Text)
+import qualified Data.HashMap.Strict as HM
+import qualified Data.ByteString.Lazy.Char8 as LBS
 
-    -- Making queries
-    activeStaff <- listStaff transport True
-    print activeStaff
+import Test.Hspec
 
--- Making queries
-data Person = Person Text Int deriving Show
+main = hspec $ do
+    describe "Transport.Mock" $ do
+        it "dispatches requests" $ do
+            t <- Mock.initTransport [ ("ping", const $ return "pong") ]
+            result <- t "ping" (document "request" empty)
+            result `shouldBe` "pong"
 
-listStaff :: Transport -> Bool -> IO [Person]
-listStaff t active = invokeWS t "urn:dummy:listStaff" () body parser
-    where
-        body = element "request" $ element "listStaff" $ do
-                   element "active" $ toXML active
-                   element "order" "age"
-                   element "limit" $ toXML (10 :: Int)
+        it "generates a soap response" $ do
+            t <- Mock.initTransport [ ("foo", Mock.handler $ \_ -> return ())]
+            result <- t "foo" (document "request" empty)
+            result `shouldBe` "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Envelope><Body/></Envelope>"
 
-        parser = StreamParser $ force "no people" $ tagNoAttr "people" $ Parse.many parsePerson
+    context "SOAP" $ do
+        it "Smoke-test with RawParser" $ do
+            t <- Mock.initTransport [ ("ping", const $ return "pong") ]
+            result <- invokeWS t "ping" () () (RawParser id)
+            result `shouldBe` "pong"
 
-        parsePerson = tagName "person" (requireAttr "age") $ \age -> do
-                          name <- Parse.content
-                          return $ Person name (read . unpack $ age)
+        describe "CursorParser" $ do
+            let salad cur = head $ cur $/ laxElement "salad"
+
+            let check parser = do
+                t <- Mock.initTransport [ ("spam", saladHandler )]
+                invokeWS t "spam" () () parser
+
+            it "reads content" $ do
+                result <- check $ CursorParser (readT "bacon" . salad)
+                result `shouldBe` "many"
+
+            it "reads and converts" $ do
+                result <- check $ CursorParser (readC "eggs" . salad)
+                result `shouldBe` (2 :: Integer)
+
+            it "reads dict" $ do
+                result <- check $ CursorParser (readDict $ laxElement "salad" )
+                result `shouldBe` HM.fromList [ ("bacon","many")
+                                              , ("sausage","some")
+                                              , ("eggs","2")
+                                              ]
+        describe "StreamParser" $ do
+            it "extracts stuff" $ do
+                let recipeParser = do
+                    ings <- Parse.force "no salad" $ Parse.tagNoAttr "salad" $ Parse.many $ Parse.tag Just return $ \name -> do
+                        quantity <- Parse.content
+                        return $ RecipeEntry (nameLocalName name) quantity
+                    return $ Recipe ings
+
+                t <- Mock.initTransport [ ("spam", saladHandler) ]
+                result <- invokeWS t "spam" () () $ StreamParser recipeParser
+                result `shouldBe` Recipe [ RecipeEntry "sausage" "some"
+                                         ,  RecipeEntry "bacon" "many"
+                                         ,  RecipeEntry "eggs" "2"
+                                         ]
+
+saladHandler = Mock.handler $ \_ -> do
+    return . element "salad" $ do
+        element "sausage" "some"
+        element "bacon" "many"
+        element "eggs" "2"
+
+data RecipeEntry = RecipeEntry Text Text deriving (Eq, Show)
+data Recipe = Recipe [RecipeEntry] deriving (Eq, Show)
