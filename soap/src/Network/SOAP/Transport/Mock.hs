35903217ad37c190b1050db27808bd9544a60777
wiz <aenor.realm@gmail.com>
2013-04-18 12:14:30 +0300

    Test soap fault cases.

--- soap/src/Network/SOAP/Transport/Mock.hs
+++ soap/src/Network/SOAP/Transport/Mock.hs
@@ -5,7 +5,7 @@ module Network.SOAP.Transport.Mock
     (
       initTransport
     , Handler, Handlers
-    , handler
+    , handler, fault
     , runQuery
     ) where
 
@@ -14,6 +14,7 @@ import Network.SOAP.Transport
 import Text.XML
 import Text.XML.Writer
 import Data.ByteString.Lazy.Char8 as LBS
+import Data.Text (Text)
 
 type Handler = Document -> IO LBS.ByteString
 type Handlers = [(String, Handler)]
@@ -40,3 +41,14 @@ handler h doc = do
            $ result
     where
         sname n = Name n (Just "http://schemas.xmlsoap.org/soap/envelope/") (Just "soapenv")
+
+-- | Emulate a SOAP fault.
+fault :: Text -- ^ SOAP Fault code (e.g. «soap:Server»)
+      -> Text -- ^ Fault string
+      -> Text -- ^ Fault detail
+      -> Handler
+fault c s d = handler . const . return $
+    element "Fault" $ do
+        element "faultcode" c
+        element "faultstring" s
+        element "detail" d
