bb0f3290d8cf7a76fc64fe84d47da3d9992b6112
wiz <aenor.realm@gmail.com>
2014-02-24 11:23:38 +0200

    + Server certificate validator can be specified for confTransport. * Re-export default validator from X509.

--- soap-tls/src/Network/SOAP/Transport/HTTP/TLS.hs
+++ soap-tls/src/Network/SOAP/Transport/HTTP/TLS.hs
@@ -1,13 +1,21 @@
 -- | SSL-enabled http transport with support for https requests and client certificates.
 
-module Network.SOAP.Transport.HTTP.TLS where
+module Network.SOAP.Transport.HTTP.TLS
+    ( confTransport
+    , makeSettings
+    -- * Certificate validation
+    , ServerCertCallback, validateDefault
+    ) where
 
+import Network.HTTP.Client (ManagerSettings)
 import Network.SOAP.Transport (Transport)
 import Network.SOAP.Transport.HTTP (confTransportWith)
 
 import Network.HTTP.Client.TLS
 import Network.TLS
-import Data.X509.Validation as X509
+import Data.X509
+import Data.X509.CertificateStore
+import Data.X509.Validation
 import Network.Connection (TLSSettings(..))
 
 import           Data.Monoid
@@ -16,13 +24,27 @@ import           Data.Default (def)
 import qualified Data.Configurator as Conf
 import           Data.Configurator.Types (Config)
 
-confTransport :: Text -> Config -> IO Transport
-confTransport section conf = do
+type ServerCertCallback = CertificateStore
+                       -> ValidationCache
+                       -> ServiceID
+                       -> CertificateChain
+                       -> IO [FailedReason]
+
+-- | Initialize a SOAP HTTP transport with HTTPS support using tls.
+confTransport :: Text   -- ^ Section name containing transport settings.
+              -> Config
+              -> ServerCertCallback
+              -> IO Transport
+confTransport section conf onSC = do
     cert <- Conf.lookup conf (section <> ".client_cert")
     key <- Conf.lookup conf (section <> ".client_key")
-    settings <- makeSettings cert key X509.validateDefault
+    settings <- makeSettings cert key onSC
     confTransportWith settings section conf id id
 
+makeSettings :: Maybe FilePath
+             -> Maybe FilePath
+             -> ServerCertCallback
+             -> IO ManagerSettings
 makeSettings (Just certFile) (Just keyFile) onSC = do
     creds <- either error Just `fmap` credentialLoadX509 certFile keyFile
 
